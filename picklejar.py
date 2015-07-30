ecee77b41f0111f62c7d44b8d777efc92cd06db2
Jesse Almanrode <isaiah1112@gmail.com>
2015-07-30 10:23:40 -0600

    Updated docstrings and copyright

--- picklejar.py
+++ picklejar.py
@@ -1,23 +1,20 @@
 # coding=utf-8
-"""
-PickleJar is a python module that allows you to work with multiple pickles while reading/writing them to a single
-file/jar.  You can load the entire jar into memory or work with pickles individually inside the jar.
-
-    Copyright (C) 2015  Jesse Almanrode
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser Public License for more details.
-
-    You should have received a copy of the GNU Lesser Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+"""PickleJar is a python module that allows you to work with multiple pickles while reading/writing them to a single
+file/jar.  You can load the entire jar into memory or work with pickles individually inside the jar."""\
+# Copyright (C) 2015 Jesse Almanrode
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Lesser General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Imports
 import pickle
