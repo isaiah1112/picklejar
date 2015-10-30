fc4676a1e9fb656d21a6c273b35b233edbaee7c3
Jesse Almanrode <isaiah1112@gmail.com>
2015-10-30 14:10:22 -0600

    Re-writing test cases.

--- tests/test_picklejar.py
+++ tests/test_picklejar.py
@@ -1,69 +1,98 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # coding=utf-8
-"""Unit Tests for picklejar Python Module"""
-__author__ = 'Jesse Almanrode'
+"""UnitTests for picklejar Python Module
+"""
 
 import os
 import sys
 import unittest
-
-here = os.path.dirname(os.path.abspath(__file__))
-project = os.path.dirname(here)
-sys.path.append(project)
 import picklejar
 
-tmploc = '/tmp/test.pjar'
+__author__ = 'Jesse Almanrode (jesse@almanrode.com)'
 
-pkldata = ['string', 1, {'dict': 'value'}]
-pklappend = ['another', 'list']
+# Globals
+testloc = '/tmp/pkljar-test.pkl'
+test_data = ['string', 1, {'dict': 'value'}]
+test_list = ['another', 'list']
 
 
 class TestPickleJar(unittest.TestCase):
 
     def setUp(self):
-        self.pkls = picklejar.Jar(tmploc)
+        """ Init a new Jar object
+        :return: Jar Object
+        """
+        self.pkls = picklejar.Jar(testloc)
+        pass
 
     def test_000_exists(self):
-        self.setUp()
-        self.assertFalse(self.pkls.exists())
+        """ Test to ensure the test Jar doesn't already exist
+        :return: Jar.exists is False
+        """
+        assert self.pkls.exists() is False
+        pass
 
     def test_001_new(self):
-        self.setUp()
-        self.assertFalse(self.pkls.exists())
-        self.pkls.collect(pkldata)
+        """ Add test_data to new pkle file
+        :return: Jar.collect() is True
+        """
+        global test_data
+        assert self.pkls.exists() is False
+        assert self.pkls.collect(test_data) is True
+        pass
 
     def test_002_append(self):
-        self.setUp()
-        self.assertTrue(self.pkls.exists())
-        self.pkls.collect(pkldata)
+        """ Test whether we can append another set of data to the Jar
+        :return: Jar.collect is True
+        """
+        global test_list
+        assert self.pkls.exists() is True
+        assert self.pkls.collect(test_list)
+        pass
 
     def test_003_read(self):
-        self.setUp()
-        self.assertTrue(self.pkls.exists())
-        self.assertIsInstance(self.pkls.dump(), list)
+        """ Test whether we can read data from the Jar
+        :return: Jar.dump() is list
+        """
+        assert self.pkls.exists() is True
+        assert self.pkls.dump() is list
+        pass
 
     def test_004_start(self):
-        self.setUp()
-        self.assertTrue(self.pkls.exists())
-        self.pkls.collect('test', newjar=True)
+        """ Overwrite an existing Jar file
+        :return: Jar.collect(newjar=True) is True
+        """
+        assert self.pkls.exists() is True
+        assert self.pkls.collect('test string', newjar=True) is True
+        pass
 
     def test_005_single(self):
-        self.setUp()
-        self.assertFalse(self.pkls._always_list_)
-        self.assertTrue(self.pkls.exists())
-        self.pkls.dump()
+        """ Return a single item from a Jar (in the test case a string) as the original type (a string)
+        :return: Jar.dump() is str
+        """
+        assert self.pkls.exists() is True
+        assert self.pkls._always_list_ is False
+        assert self.pkls.dump() is str
+        pass
 
     def test_006_single_list(self):
-        self.setUp()
+        """ Return a single item from a Jar (in the test case a string) as a list with a single item (the string)
+        :return: Jar.dump() is list
+        """
+        assert self.pkls.exists() is True
         self.pkls._always_list_ = True
-        self.assertTrue(self.pkls.exists())
-        self.assertIsInstance(self.pkls.dump(), list)
+        assert self.pkls._always_list_ is True
+        assert self.pkls.dump() is list
+        pass
 
     def test_007_remove(self):
-        self.setUp()
-        self.assertTrue(self.pkls.exists())
-        self.pkls.remove()
-        self.assertFalse(self.pkls.exists())
+        """ Remove an existing Jar file
+        :return: Jar.remove is True
+        """
+        assert self.pkls.exists() is True
+        assert self.pkls.remove() is True
+        assert self.pkls.exists() is False
+        pass
 
 
 if __name__ == '__main__':
