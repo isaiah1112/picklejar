e3d9e9fffba10ba2076c17731eb7f376b5827fb9
Jesse Almanrode <isaiah1112@gmail.com>
2016-01-07 20:06:41 -0700

    Changed Jar.dump to Jar.load and Jar.collect to Jar.dump to better match pickle methods.

--- picklejar.py
+++ picklejar.py
@@ -55,7 +55,7 @@ class Jar(object):
         else:
             return False
 
-    def dump(self, always_list=False):
+    def load(self, always_list=False):
         """Dumps all the pickles out of the file/jar 
 
         :param always_list: Ensure that Jars with single pickle return as a list (default = False)
@@ -77,8 +77,8 @@ class Jar(object):
         else:
             return _pickles
 
-    def collect(self, pickles, newjar=False, collapse=False):
-        """Pickle an item or list of items to a file/jar.
+    def dump(self, pickles, newjar=False, collapse=False):
+        """Add a Pickle to the file/jar.
 
         :param pickles: Item or list of items to pickle
         :param newjar: Start a new jar (default = False)
