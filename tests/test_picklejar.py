d051fd4328fdc786ef34f49a3f6a695dfd56ce24
Jesse Almanrode <isaiah1112@gmail.com>
2016-01-07 20:07:00 -0700

    Updated unittests.

--- tests/test_picklejar.py
+++ tests/test_picklejar.py
@@ -17,7 +17,7 @@ __author__ = 'Jesse Almanrode (jesse@almanrode.com)'
 
 # Globals
 testloc = '/tmp/pkljar-test.pkl'
-test_data = ['string', 1, {'dict': 'value'}]
+test_data = ['string', 1, {'key': 'value'}]
 test_list = ['another', 'list']
 
 
@@ -34,89 +34,89 @@ class TestPickleJar(unittest.TestCase):
         """ Test to ensure the test Jar doesn't already exist
         :return: Jar.exists is False
         """
-        assert self.pkls.exists() is False
+        self.assertFalse(self.pkls.exists())
         pass
 
     def test_001_new(self):
         """ Add test_data to new pkle file
-        :return: Jar.collect() is True
+        :return: Jar.dump() is True
         """
         global test_data
-        assert self.pkls.exists() is False
-        assert self.pkls.collect(test_data) is True
+        self.assertFalse(self.pkls.exists())
+        self.assertTrue(self.pkls.dump(test_data))
         pass
 
     def test_002_append(self):
         """ Test whether we can append another set of data to the Jar
-        :return: Jar.collect is True
+        :return: Jar.dump() is True
         """
         global test_list
-        assert self.pkls.exists() is True
-        assert self.pkls.collect(test_list)
+        self.assertTrue(self.pkls.exists())
+        self.assertTrue(self.pkls.dump(test_list))
         pass
 
     def test_003_read(self):
         """ Test whether we can read data from the Jar
-        :return: Jar.dump() is list
+        :return: Jar.load() is list
         """
-        assert self.pkls.exists() is True
-        assert isinstance(self.pkls.dump(), list)
+        self.assertTrue(self.pkls.exists())
+        self.assertIsInstance(self.pkls.load(), list)
         pass
 
     def test_004_start(self):
         """ Overwrite an existing Jar file
-        :return: Jar.collect(newjar=True) is True
+        :return: Jar.dump(newjar=True) is True
         """
-        assert self.pkls.exists() is True
-        assert self.pkls.collect('test string', newjar=True) is True
+        self.assertTrue(self.pkls.exists())
+        self.assertTrue(self.pkls.dump('test string', newjar=True))
         pass
 
     def test_005_single(self):
         """ Return a single item from a Jar (in the test case a string) as the original type (a string)
-        :return: Jar.dump() is str
+        :return: Jar.load() is str
         """
-        assert self.pkls.exists() is True
-        assert self.pkls.always_list is False
-        assert isinstance(self.pkls.dump(), (str,))
+        self.assertTrue(self.pkls.exists())
+        self.assertFalse(self.pkls.always_list)
+        self.assertIsInstance(self.pkls.load(), str)
         pass
 
     def test_006_single_list_property(self):
         """ Return a single item from a Jar (in the test case a string) as a list with a single item (the string)
-        :return: Jar.dump() is list
+        :return: Jar.load() is list
         """
-        assert self.pkls.exists() is True
+        self.assertTrue(self.pkls.exists())
         self.pkls.always_list = True
-        assert self.pkls.always_list is True
-        assert isinstance(self.pkls.dump(), list)
+        self.assertTrue(self.pkls.always_list)
+        self.assertIsInstance(self.pkls.load(), list)
         pass
 
     def test_007_single_list(self):
         """ Return a single item from a Jar (in the test case a string) as a list with a single item (the string)
-        :return: Jar.dump(always_list=True) is list
+        :return: Jar.load(always_list=True) is list
         """
-        assert self.pkls.exists() is True
+        self.assertTrue(self.pkls.exists())
         self.pkls.always_list = False
-        assert self.pkls.always_list is False
-        assert isinstance(self.pkls.dump(always_list=True), list)
+        self.assertFalse(self.pkls.always_list)
+        self.assertIsInstance(self.pkls.load(always_list=True), list)
         pass
 
     def test_008_collapse(self):
         """ Ensure a list of objects is written as a single pickle object
-        :return: len(self.pkls.dump()) == 2
+        :return: len(self.pkls.load()) == 2
         """
-        assert self.pkls.exists() is True
-        assert self.pkls.collect(test_list, newjar=True, collapse=True) is True
-        assert isinstance(self.pkls.dump(), list)
-        assert len(self.pkls.dump()) == 2
+        self.assertTrue(self.pkls.exists())
+        self.assertTrue(self.pkls.dump(test_list, newjar=True, collapse=True))
+        self.assertIsInstance(self.pkls.load(), list)
+        self.assertEqual(len(self.pkls.load()), 2)
         pass
 
     def test_009_remove(self):
         """ Clean up after all tests complete
         :return: Jar.remove is True
         """
-        assert self.pkls.exists() is True
-        assert self.pkls.remove() is True
-        assert self.pkls.exists() is False
+        self.assertTrue(self.pkls.exists())
+        self.assertTrue(self.pkls.remove())
+        self.assertFalse(self.pkls.exists())
         pass
 
 if __name__ == '__main__':
