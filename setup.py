50c9a55a089f4c1442d039f4079f8986f5efdaab
Jesse Almanrode <isaiah1112@gmail.com>
2016-01-15 08:36:53 -0700

    Bumped version to 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(name='picklejar',
-      version='2.0.1b',
+      version='2.0.1',
       description='Read and write multiple pickles to a single file',
       author='Jesse Almanrode',
       author_email='jesse@almanrode.com',
