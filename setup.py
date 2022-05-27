69d4e3c124ef8cc15b9749b771c83d0cd76d59f1
Jesse Almanrode <jesse@almanrode.com>
2022-05-27 10:59:08 -0600

    Updated setup.py file to pull in README, set python_requires, etc.

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python2
 # coding=utf-8
 """Setup script for picklejar Python module"""
-
 from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(name='picklejar',
       version='2.1.2',
-      description='Read and write pickles to a single file',
       author='Jesse Almanrode',
       author_email='jesse@almanrode.com',
+      description='Read and write pickles to a single file',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
       url='http://picklejar.readthedocs.io/',
       py_modules=['picklejar'],
-      license='GNU Lesser General Public License v3 or later (LGPLv3+)',
-      install_requires=['dill',
-                        ],
+      python_requires='>=3.7',
+      install_requires=['dill', ],
       platforms='any',
       classifiers=[
           'Programming Language :: Python',
@@ -21,10 +24,6 @@ setup(name='picklejar',
           'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
           'Development Status :: 5 - Production/Stable',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
