6ee85b56cf4ade1bc8e978d4035c71191c73537f
Jesse Almanrode <jesse@almanrode.com>
2022-10-27 13:22:09 -0600

    Bumping version numbers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(name='picklejar',
-      version='3.0.0',
+      version='3.0.1',
       author='Jesse Almanrode',
       author_email='jesse@almanrode.com',
       description='Read and write pickles to a single file',
@@ -28,6 +28,7 @@ setup(name='picklejar',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
           'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules',
